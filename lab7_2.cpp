3903e6340f38cfe682a63403413325a88df1a605
phuennae <liverpoolnae@gmail.com>
2022-12-13 16:14:54 +0700

    lab7_2 complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,26 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k, result=0;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
-	
+	cout<<"Please input k: ";
+	cin>>k;
+	if(k<=0){
+		cout<<"Invalid input!!!";
+	}else{
+		int i=0;
+		while(i<5000){	
+			if(P[i]%k==0){
+				result = result-P[i];
+			}else{
+				result = result+P[i];
+			}
+			i++;
+		}
+		cout<<"Result = "<<result;
+	}
 	return 0;
 }
